8c2939712bff33db020ace737c60cdb6f04fcd26
betanaijaboi <budoessien2331@outlook.com>
2026-07-26 00:49:59 +0100

    Remove view-count eye icon from magazine cards

--- public/app.js
+++ public/app.js
@@ -187,12 +187,6 @@ function renderMagazines() {
       <div class="card-info">
         <div class="card-title">${esc(mag.title)}</div>
         ${mag.issue ? `<div class="card-issue">${esc(mag.issue)}</div>` : ''}
-        <div class="card-meta">
-          <span class="card-views">
-            <svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/></svg>
-            ${mag.views || 0}
-          </span>
-        </div>
       </div>
     </article>`).join('');
 }
